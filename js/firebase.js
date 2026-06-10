1a19ad4236d9cf2208f9b2d932ce32bec11b6a2e
subhickshanas <subhickshanas@gmail.com>
2026-06-10 10:27:54 +0530

    Create Firebase configuration file

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDemoKey123456789",
+    authDomain: "sweet-bakery.firebaseapp.com",
+    projectId: "sweet-bakery-demo",
+    storageBucket: "sweet-bakery-demo.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
